a24656b017b33a9a70bc2e9ae6a0a3fb789b4f7c
RiccardoParrino <riccardoparrino1@gmail.com>
2025-07-26 17:50:16 +0200

    adding backend to the project

---
+++ backend/mycrm/src/test/java/parrino/riccardo/mycrm/MycrmApplicationTests.java
@@ -0,0 +1,13 @@
+package parrino.riccardo.mycrm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MycrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
