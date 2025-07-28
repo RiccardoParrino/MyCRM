a3c53579130954ad83053cc9ac4bcce19ccf6429
RiccardoParrino <riccardoparrino1@gmail.com>
2025-07-28 13:08:42 +0200

    cors for all endpoints

--- backend/mycrm/src/main/java/parrino/riccardo/mycrm/authentication/WebConfig.java
+++ backend/mycrm/src/main/java/parrino/riccardo/mycrm/authentication/WebConfig.java
@@ -13,7 +13,7 @@ public class WebConfig {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/readCustomer")
+                registry.addMapping("/**")
                         .allowedOrigins("http://localhost:4200")
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowedHeaders("*");
