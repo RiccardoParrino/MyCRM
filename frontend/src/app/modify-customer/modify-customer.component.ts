4418963cfc89ed82f25b2aa38c746842fb49eec9
RiccardoParrino <riccardoparrino1@gmail.com>
2025-07-30 12:10:18 +0200

    adding delete and modify dialog

---
+++ frontend/src/app/modify-customer/modify-customer.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-modify-customer',
+  imports: [],
+  templateUrl: './modify-customer.component.html',
+  styleUrl: './modify-customer.component.css'
+})
+export class ModifyCustomerComponent {
+
+}
