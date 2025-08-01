6e32a495cce7d48138687d63ba37f31471b89285
RiccardoParrino <riccardoparrino1@gmail.com>
2025-08-01 11:08:05 +0200

    CRM-21 adding login component

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -1,11 +1,30 @@
 import { Component } from '@angular/core';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {FormsModule} from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../service/auth.service';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [MatInputModule, MatFormFieldModule, FormsModule, MatButtonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+
+  username = '';
+  password = '';
+
+  constructor(private authService:AuthService, private router:Router) {}
+
+  onSubmit() {
+    if (this.authService.login(this.username, this.password)) {
+      this.router.navigate(['customer']);
+    } else {
+      alert('Credenziali errate');
+    }
+  }
 
 }
