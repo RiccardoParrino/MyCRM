a50bc838976cc6341d5b482ac4640e2eef5816f9
RiccardoParrino <riccardoparrino1@gmail.com>
2025-08-01 11:07:50 +0200

    CRM-21 adding route to login

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,28 +3,48 @@ import { CustomerComponent } from './customer/customer.component';
 import { ProductComponent } from './product/product.component';
 import { SaleComponent } from './sale/sale.component';
 import { SettingComponent } from './setting/setting.component';
+import { LoginComponent } from './login/login.component';
+import { authGuard } from './auth.guard';
 
 export const routes: Routes = [
 
+    {   path: '',
+        redirectTo: 'login', 
+        pathMatch: 'full' 
+    },
+    {
+        path: 'login',
+        component: LoginComponent,
+        title: "Login Page",
+        canActivate: [authGuard]
+    },
     {
         path: 'customers',
         component: CustomerComponent,
-        title: "Customers Page"
+        title: "Customers Page",
+        canActivate: [authGuard]
     },
     {
         path: 'products',
         component: ProductComponent,
-        title: "Products Page"
+        title: "Products Page",
+        canActivate: [authGuard]
     },
     {
         path: 'sales',
         component: SaleComponent,
-        title: "Sales Page"
+        title: "Sales Page",
+        canActivate: [authGuard]
     },
     {
         path: 'settings',
         component: SettingComponent,
-        title: "Settings Page"
+        title: "Settings Page",
+        canActivate: [authGuard]
+    },
+    {
+        path: '**',
+        redirectTo: 'login'
     }
 
 ];
