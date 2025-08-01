89162165a780610b7b8ebff3924260b07f4c79aa
RiccardoParrino <riccardoparrino1@gmail.com>
2025-08-01 11:08:21 +0200

    CRM-21 adding ngIf

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -3,13 +3,15 @@ import { RouterOutlet } from '@angular/router';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
 import { RouterModule } from '@angular/router';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MatSidenavModule, MatListModule, RouterModule],
+  imports: [RouterOutlet, MatSidenavModule, MatListModule, RouterModule, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'MyCrm';
+  showAppComponentContainer: boolean = false;
 }
