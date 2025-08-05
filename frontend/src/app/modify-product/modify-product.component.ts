d3a1a544dd4c5428bd553088e28c22cd78fd848b
RiccardoParrino <riccardoparrino1@gmail.com>
2025-08-05 15:12:53 +0200

    CRM-51 generate modify-product component

---
+++ frontend/src/app/modify-product/modify-product.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-modify-product',
+  imports: [],
+  templateUrl: './modify-product.component.html',
+  styleUrl: './modify-product.component.css'
+})
+export class ModifyProductComponent {
+
+}
