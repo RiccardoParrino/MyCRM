b3b5550729a43c7569e8a03e9688f03d309d987c
RiccardoParrino <riccardoparrino1@gmail.com>
2025-08-07 08:39:35 +0200

    CRM-58 adding dep and config

--- backend/mycrm/src/main/java/parrino/riccardo/mycrm/authentication/WebConfig.java
+++ backend/mycrm/src/main/java/parrino/riccardo/mycrm/authentication/WebConfig.java
@@ -2,10 +2,14 @@ package parrino.riccardo.mycrm.authentication;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableWebSecurity
 public class WebConfig {
 
     @Bean
@@ -19,6 +23,21 @@ public class WebConfig {
                         .allowedHeaders("*");
             }
         };
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .authorizeHttpRequests((requests) -> requests
+                .requestMatchers("/", "/login").permitAll()
+                .anyRequest().authenticated()
+            )
+            .formLogin((form) -> form
+                .loginPage("/login")
+                .permitAll())
+            .logout((logout)->logout.permitAll());
+
+        return http.build();
     }
 
 }
